5220de2c7ea5bcfc2235fa232f0dc9d2acb3f5ed
Rasanzzz <rasangahunia@gmail.com>
2025-12-25 16:11:03 +1300

    new deplot.yml

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/postcss'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/',
   plugins: [react()],
   css: {
     postcss: {
